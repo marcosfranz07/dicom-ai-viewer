4a3db3dd45ac671e6819f5268051f193b0d7f291
unknown <marcosfranz.0107@gmail.com>
2025-06-22 12:57:22 +0800

    Add static and templates folders, update main.py

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, DICOM AI World!"}
+
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    print("🔵 Home page route hit!")  # Debug line
+    return templates.TemplateResponse("index.html", {"request": request})
